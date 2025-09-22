db7465b0800c4382d378b35ce46ee5faea44b5b3
Shubhi Verma <verma@Shubhis-MacBook-Pro.local>
2025-09-22 10:31:52 +0530

    video call icon fix

--- frontend/src/components/CallButton.jsx
+++ frontend/src/components/CallButton.jsx
@@ -2,7 +2,7 @@ import { VideoIcon } from 'lucide-react'
 
 const CallButton = ({ handleVideoCall }) => {
   return (
-    <div className='p-3 border-b items-center justify-end max-w-7xl ml-auto w-full absolute top-0 left-[1100px]'>
+    <div className='p-3 border-b items-center justify-end max-w-7xl ml-auto w-full absolute top-0 left-[90%]'>
         <button onClick={handleVideoCall} className='btn btn-success btn-sm text-white'>
             <VideoIcon className='size-6' />
         </button>
